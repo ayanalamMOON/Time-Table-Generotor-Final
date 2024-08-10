dd8041d529a322d3bc8a1a5d6c5bace93b4508a5
Ayan Alam <83778128+ayanalamMOON@users.noreply.github.com>
2024-08-10 19:17:38 +0530

    Added model.py to the backend folder

---
+++ Backend/model.py
@@ -0,0 +1,43 @@
+from typing import List
+from pydantic import BaseModel, Field
+from bson import ObjectId
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls)
+        yield cls.validate
+
+    @clssmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid ObjectId")
+        return ObjectId(v)  # pragma: no cover
+    
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class CeateCourse(BaseModel):
+    name: str
+    lectureno : int 
+    duration : int 
+    instructor_name: str
+    start_hr: int
+    end_hr: int
+
+
+class Course(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias ="_id")
+    name: str
+    lectureno: int
+    duration: int
+    instructor_name: str
+    start_hr: int
+    end_hr: int
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
